d536e6bccf06a7c4075202df46a73d1def518466
febi <febianafs@gmail.com>
2023-12-17 23:21:41 +0700

    success login register

--- ARTourguide/app/src/main/java/com/capstone/ar_tourguide/onboarding/OnBoardingActivity2.kt
+++ ARTourguide/app/src/main/java/com/capstone/ar_tourguide/onboarding/OnBoardingActivity2.kt
@@ -1,10 +1,13 @@
 package com.capstone.ar_tourguide.onboarding
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.capstone.ar_tourguide.LoginActivity
 import com.capstone.ar_tourguide.R
 
 class OnBoardingActivity2 : AppCompatActivity() {
@@ -20,5 +23,12 @@ class OnBoardingActivity2 : AppCompatActivity() {
         animationDrawable.start()
 
         supportActionBar?.hide()
+
+        val startedButton = findViewById<Button>(R.id.started_button)
+
+        startedButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
